345c7373b1900d1b1a9701c702a4c4358d3d1a4f
DavidCharif <53876827+DavidCharif@users.noreply.github.com>
2021-09-30 10:26:52 -0500

    Create registro.js
    Se crea el registro.js y se añade la clase de Usuario junto con su constructor

---
+++ scripts/registro.js
@@ -0,0 +1,13 @@
+let listadeUsuarios = [];
+
+
+class Usuario{
+    constructor(nombre,apellido,telefono,correo,password){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.password = password;
+    }
+   
+}
