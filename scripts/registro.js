645bdbe11e0b7dce8a89239dbb79507683a5e961
hrincon <hrincon@localdomain>
2021-09-30 19:03:45 -0500

    Agregada función de arrays

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,15 +1,17 @@
-let listadeUsuarios = [];
+let listadeUsuarios = [];   // Este es el nombre que se le dio a la lista
 
-
-class Usuario{
-    constructor(nombre,apellido,telefono,correo,password){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.password = password;
+/* La siguiente es una forma de crear objetos y hacer arrays que me pareció más
+eficiente, comentarios son siempre bienvenidos. Aclarar si los id están correctos */
+function agregarRegistro() {
+    let usuario = {
+        first_name: document.getElementById("nombre").value,
+        last_name: document.getElementById("apellidos").value,
+        phone_number: document.getElementById("telefono").value,
+        email: document.getElementById("correo").value,
+        user_password: document.getElementById("password").value
     }
-   
+    listadeUsuarios.push(usuario);
+    document.forms[0].reset();  // Limpia los campos del registro
 }
 
 // FUNCION FILTRA CORREOS GMAIL.COM
