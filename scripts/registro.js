ab30cd58b7921ee760a580ad9daa0356d22caba2
David Charif <davidcharif@hotmail.com>
2021-10-01 07:50:25 -0500

    Correccion module exports, se agrega un parametro en la funcion filtrar correo

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,23 +1,27 @@
-let listadeUsuarios = [];   // Este es el nombre que se le dio a la lista
+var registros = [];   // Este es el nombre que se le dio a la lista
 
 /* La siguiente es una forma de crear objetos y hacer arrays que me pareció más
 eficiente, comentarios son siempre bienvenidos. Aclarar si los id están correctos */
 function agregarRegistro() {
+     
     let usuario = {
-        first_name: document.getElementById("nombre").value,
-        last_name: document.getElementById("apellidos").value,
-        phone_number: document.getElementById("telefono").value,
-        email: document.getElementById("correo").value,
-        user_password: document.getElementById("password").value
+        nombre  : document.getElementById("nombre").value,
+        apellido: document.getElementById("apellidos").value,
+        telefono: document.getElementById("telefono").value,
+        correo: document.getElementById("correo").value,
+        contrasena: document.getElementById("password").value
     }
     listadeUsuarios.push(usuario);
-    document.forms[0].reset();  // Limpia los campos del registro
+     // Limpia los campos del registro
 }
 
 // FUNCION FILTRA CORREOS GMAIL.COM
 function filtrarCorreo(){
-    let gmail = listadeUsuarios.filter(UsuarioGmail => UsuarioGmail.correo.includes('@gmail.com'));
+    let gmail = listadeUsuarios.filter(UsuarioGmail => UsuarioGmail.email.includes('@gmail.com'));
     console.log(gmail);
 }
 
-module.exports={agregarRegistro,filtrarCorreo};
+module.exports.registros = registros;
+module.exports.ordenarArreglo = ordenarArreglo;
+module.exports.filtrarCorreo = filtrarCorreo;
+module.exports.agregarRegistro = agregarRegistro;
