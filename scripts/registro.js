cc8b68e97a60c9b1b9daa12e8a5a65076e20c26f
DavidCharif <53876827+DavidCharif@users.noreply.github.com>
2021-10-01 19:04:46 -0500

    Update registro.js
    se cambian las llaves por el id en Agregar registro/ usuario

--- scripts/registro.js
+++ scripts/registro.js
@@ -4,11 +4,11 @@ let registros = [];   // Nombre según nuevo instructivo
 function agregarRegistro() {
      
     let usuario = {
-        first_name: document.getElementById("nombre").value,
-        last_name: document.getElementById("apellido").value,
-        phone_number: document.getElementById("telefono").value,
-        email: document.getElementById("correo").value,
-        user_password: document.getElementById("contrasena").value
+        nombre: document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        telefono: document.getElementById("telefono").value,
+        correo: document.getElementById("correo").value,
+        contrasena: document.getElementById("contrasena").value
     }
     registros.push(usuario);
     // document.forms[0].reset(); lo dejamos comentado para que no moleste
@@ -17,12 +17,12 @@ function agregarRegistro() {
 // FUNCION FILTRA CORREOS GMAIL.COM
 function filtrarCorreo(array){
     //se agrega variable
-    let gmail = array.filter(UsuarioGmail => UsuarioGmail.email.includes('@gmail.com'));
+    let gmail = array.filter(UsuarioGmail => UsuarioGmail.correo.includes('@gmail.com'));
     console.log(gmail);
     return gmail;
 }
 
 module.exports.registros = registros; // Este no me cuadra
 module.exports.filtrarCorreo = filtrarCorreo;
-module.exports.ordenarArreglo = ordenarArreglo;
+/*module.exports.ordenarArreglo = ordenarArreglo;*/
 module.exports.agregarRegistro = agregarRegistro;
