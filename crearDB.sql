ff66f9a482dc2ecd89cadc7aca27179c6c484059
jaimevalenciap <33964088+jaimevalenciap@users.noreply.github.com>
2021-10-06 05:27:36 -0500

    Create crearDB.sql

---
+++ crearDB.sql
@@ -0,0 +1,9 @@
+create database proyecto
+
+create table usuarios(
+   nombre varchar(30) not null,
+   apellido varchar(30) not null,
+   telefono int(7) not null,
+   correo varchar(60) not null,
+   password varchar() not null
+);
