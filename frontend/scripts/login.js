d6334de6b4dda8ae030103831ca1f681bd24965a
David Charif <davidcharif@hotmail.com>
2021-10-18 16:52:10 -0500

    Validado en 10 por el bot

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,52 +1,49 @@
-let registros = [];
+let registros = [{ correo: "juan@gmail.com", contrasena: "SecurePassword123" }];
 
-class Usuario{
-    constructor(nombre,apellido,telefono,correo,contrasena){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.contrasena = contrasena;
-    };
-}
 
 function agregarRegistro() {
     
-    nombre = document.getElementById("nombre").value,
-        apellido = document.getElementById("apellido").value,
-        telefono = document.getElementById("telefono").value,
-        correo = document.getElementById("correo").value,
-        contrasena = document.getElementById("contrasena").value
-    let usuario = new Usuario(nombre,apellido,telefono,correo,contrasena);
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let contrasena = document.getElementById("contrasena").value;
+    let usuario = {nombre,apellido,telefono,correo,contrasena};
     
     registros.push(usuario);
     // document.forms[0].reset(); lo dejamos comentado para que no moleste
 }
 
-function login(){ 
-var correo = document.getElementById("correo").value;
-var contrasena = document.getElementById("contrasena").value;
-var captcha= document.getElementById("captcha").value;
-    for(let i=0; i<registros.length;i++){
-    if(registros[i].correo==correo && registros[i].contrasena==contrasena){
-        validarCAPTCHA(captcha); 
-        return true;
-        
+function login() {
+    let correo = document.getElementById("correo").value; 
+    let contrasena = document.getElementById("contrasena").value;    
+    let usuario = registros.some(usuario => usuario.correo === correo && usuario.contrasena === contrasena);
+        if (usuario === true) {            
+            return true;
+            
         }
-    }
-       
+        else {
+            console.log("False");
+        return false;
+        }
 }
 
-function validarCAPTCHA(valor){
-    if (valor == (4/(3/12000))){
+
+function validarCAPTCHA(valor) {
+   let correctAnswer = 1000;
+
+    if (valor == correctAnswer) {
         return true;
     } else {
-        return false;
+    return false;
     }
+
+
+
 }
 
-
-module.exports.login = login;
 module.exports.registros = registros;
+module.exports.login = login;
+
 module.exports.validarCAPTCHA = validarCAPTCHA;
 module.exports.agregarRegistro = agregarRegistro;
